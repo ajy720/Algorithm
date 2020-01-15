1e58ae1f6e2737a2bee35a43f1dbe27a1c0c2897
HomeDesktop <ajy720@gmail.com>
2020-01-16 00:37:40 +0900

    BOJ 1016

--- BOJ/1016.py
+++ BOJ/1016.py
@@ -15,6 +15,8 @@ for i in range(2, MAX+1):
         for j in range(i*2, MAX+1, i):
             filt[j] = False
 
+del filt
+
 for i in num:
     #floor(max/i) - ceil(min/i) + 1 : max와 min 사이에 i의 배수가 몇 개 있는지 구하는 식.
     for j in range(ceil(min/i), floor(max/i)+1):
