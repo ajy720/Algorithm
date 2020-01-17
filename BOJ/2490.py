85662060c76537b2ff9388d85bd5f218f0cc6c0e
HomeDesktop <ajy720@gmail.com>
2020-01-18 04:21:52 +0900

    BOJ 2490

---
+++ BOJ/2490.py
@@ -0,0 +1,16 @@
+for _ in range(3):
+    a = sum(list(map(int, input().split())))
+    if a == 3:
+        print("A")
+        
+    if a == 2:
+        print("B")
+        
+    if a == 1:
+        print("C")
+        
+    if a == 0:
+        print("D")
+        
+    if a == 4:
+        print("E")
