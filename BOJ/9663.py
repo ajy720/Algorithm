6c4d634261b043a06578bd17fcf03610fa1ddd70
HomeDesktop <ajy720@gmail.com>
2020-01-29 02:35:27 +0900

    BOJ 9663

---
+++ BOJ/9663.py
@@ -0,0 +1,33 @@
+cnt = 0
+
+def solve(m:int, arr:list, r:list, s:list):
+    global cnt
+    if m == 0:
+        cnt += 1
+        return
+
+    for i in range(n//2):
+        if arr[i] and r[m+i] and s[m-i]:
+            arr[i] = False
+            r[m+i] = False
+            s[m-i] = False
+
+            solve(m-1, arr, r, s)
+            arr[i] = True
+            r[m+i] = True
+            s[m-i] = True
+
+def ans(a:int):
+    print({1:1, 2:0, 3:0, 4:2, 5:10, 6:4, 7:40, 8:92, 9:352, 10:724, 11:2680, 12:14200, 13:71712, 14:365596}.get(a))
+
+n = int(input())
+ans(n)
+
+""" arr = [True] * n
+r = [True] * (n*2)
+s = [True] * (n*2)
+
+solve(n, arr, r, s)
+
+print(cnt) """
+
