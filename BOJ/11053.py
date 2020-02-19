c84abdd45ad318d29f7d93f88001f376d6e8133b
AnHyeonSeok <ajy720@gmail.com>
2020-02-20 01:10:59 +0900

    BOJ 11053

---
+++ BOJ/11053.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+ans = [1] * n
+
+for i in range(n):
+    try:
+        ans[i] = max([ans[j] for j in range(0, i) if arr[j] < arr[i]]) + 1
+    except:
+        continue
+
+print(max(ans))
+    
