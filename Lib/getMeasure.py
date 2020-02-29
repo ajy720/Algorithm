792248a6860fe4b3b673391af2c715c736ce5e4d
AnHyeonSeok <ajy720@gmail.com>
2020-03-01 01:06:02 +0900

    Others

--- Lib/getMeasure.py
+++ Lib/getMeasure.py
@@ -1,12 +1,10 @@
 def getMeasure(n:int):
     f = []
     sq = int(n**(1/2))
-    for i in range(2, sq):
+    for i in range(2, sq+1):
         if n % i == 0:
-          f[len(f)//2-1:len(f)//2] += [i, n//i]
-
-    if n % sq == 0:
-        f.insert(len(f)//2, sq)
+            if n//i == sq: f.insert(len(f)//2, sq)
+            else: f[len(f)//2-1:len(f)//2] += [i, n//i]
 
     return f + [n]
 
