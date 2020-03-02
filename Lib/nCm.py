91ddc538301a520157f903e363a605b6ea8a039f
AnHyeonSeok <ajy720@gmail.com>
2020-03-02 18:06:05 +0900

    Personal Library nCm

---
+++ Lib/nCm.py
@@ -0,0 +1,19 @@
+def nCm(n, m):
+    if m == 0: return 1
+    if m > n or m < 0: return 0
+
+    if m > n//2: m = n-m
+
+    son = mom = 1
+
+    for i in range(n, n-m, -1): son *= i
+    for i in range(1, m+1, 1): mom *= i
+
+    return son//mom
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+
+    c = nCm(n, m)
+
+    print(c)
