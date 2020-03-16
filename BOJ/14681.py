3ed450721e50ae837749980cdcc9865c5ce179f6
HomeDesktop <ajy720@gmail.com>
2020-03-16 23:24:46 +0900

    BOJ 14681

---
+++ BOJ/14681.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+if a * b > 0:
+    if a > 0:
+        print(1)
+    else:
+        print(3)
+else:
+    if a > 0:
+        print(4)
+    else:
+        print(2)
