0a4306f0c64b4410ee7772ffe819e039907ce74d
HomeDesktop <ajy720@gmail.com>
2020-03-30 03:39:26 +0900

    update segmentTree

--- Lib/segmentTree.py
+++ Lib/segmentTree.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     n = int(input("자료의 갯수를 입력하세요. : "))
     a = [int(input(f"{i}번쨰 자료를 입력하세요. : ")) for i in range(n)]
-    tree = [None] * ((math.ceil(math.log(n, 2))+1)**2)
+    tree = [None] * (2**(math.ceil(math.log(n, 2))+1))
 
     init(1, 0, n-1)
 
