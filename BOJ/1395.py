a1c720aad45f3f14db8d62c60e8cd76f78325e43
HomeDesktop <ajy720@gmail.com>
2020-04-02 22:38:55 +0900

    BOJ 1395

--- BOJ/1395.py
+++ BOJ/1395.py
@@ -1,70 +1,67 @@
 import sys, math
 
-#input = sys.stdin.readline
-def propagtion(node):
-    pass
+input = sys.stdin.readline
+
+def propagtion(node, start, end):
+    if lazy[node]:
+        lazy[node] = False
+        if not start == end:
+            lazy[node*2]^=True
+            lazy[node*2+1]^=True
+        tree[node] = end - start + 1 - tree[node]
 
 def init(node, start, end):
     if start == end: # 리프노드라면 데이터를 추가
-        tree[node] = a[start]
-        return a[start] # 부모 노드에게 값 전달
+        tree[node] = 0
+        return 0 # 부모 노드에게 값 전달
     
-    else: # 리프 노드가 아닌 경우 좌우 자식으로 가지를 뻗고, 두 자식의 합을 저장. 이 부분에서 구간합, 최소/최대값 세그먼트 트리 등 속성이 결정된다.
-        tree[node] = init(node*2, start, (start+end)//2) + init(node*2+1, (start+end)//2+1, end)
+    else: # 리프 노드가 아닌 경우 좌우 자식으로 가지를 뻗고, 두 자식의 합을 저장.
+        l = init(node*2, start, (start+end)//2)
+        r = init(node*2+1, (start+end)//2+1, end)
+        tree[node] = l + r
+
         return tree[node] # 부모노드에게 값 전달
 
 def query(node, start, end, left, right):
-    if left > end or right < start:
+    propagtion(node, start, end)
+
+    if left > end or right < start: 
         return 0
-    
-    elif start == end:
-        if lazy[node]:
-            lazy[node] = False
-            tree[node]^=1
 
-        return tree[node]
-
-    elif start >= left and end <= right:
-        if lazy[node]:
-            tree[node] = end - start + 1 - tree[node]
-            propagtion(node)
-            lazy[node] = False
-        
+    elif start >= left and end <= right: 
         return tree[node]
 
     else:
-        if lazy[node]:
-            propagtion(node)
-            lazy[node] = False
-            
-        left = query(node*2, start, (start+end)//2, left, right)
-        right = query(node*2+1, (start+end)//2+1, end, left, right)
+        l = query(node*2, start, (start+end)//2, left, right)
+        r = query(node*2+1, (start+end)//2+1, end, left, right)
 
-        return left + right
+        return l+r
         
-
 def update(node, start, end, left, right):
-    if left > end or right < start:
-        return
+    propagtion(node, start, end)
+            
+    if left > end or right < start: 
+        return tree[node]
 
     elif start == end:
-        lazy[node] ^= True
+        tree[node] ^= 1
+        return tree[node]
 
     elif start >= left and end <= right:
-        lazy[node] ^= True
+        lazy[node] = True
+        return end - start + 1 - tree[node]
     
     else:
-        if lazy[node]:
-            propagtion(node)
+        l = update(node*2, start, (start+end)//2, left, right)
+        r = update(node*2+1, (start+end)//2+1, end, left, right)
+        tree[node] = l + r
 
-        update(node*2, start, (start+end)//2, left, right)
-        update(node*2+1, (start+end)//2+1, end, left, right)
+        return tree[node]
 
 if __name__ == "__main__":
-    global tree, a, n
+    global tree, n
     n, m = map(int, input().split())
 
-    a = [0]*(n+1)
     tree = [None] * (2**math.ceil(math.log(n, 2)+1))
     lazy = [False] * (2**math.ceil(math.log(n, 2)+1))
 
