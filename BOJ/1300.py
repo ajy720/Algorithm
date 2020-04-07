2c4a56038f421d8552660c8beb3b3a244d34d932
AnHyeonSeok <ajy720@gmail.com>
2020-04-07 13:53:42 +0900

    BOJ 1300 optimize

--- BOJ/1300.py
+++ BOJ/1300.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
 
     while left <= right:
         mid = (left + right)//2
-        cnt = 0
+        cnt = n * (mid//n) # i행 까지의 최대값은 i*j이니까, mid보다 작은 것들은 검사하지 않고 카운트
 
-        for i in range(n):
-            cnt += min(mid//(i+1), n)
+        for i in range(mid//n+1, n+1): # 검사 안 한 부분만 반복 검사
+            cnt += mid//i # 
 
         if cnt < k:
             left = mid + 1
