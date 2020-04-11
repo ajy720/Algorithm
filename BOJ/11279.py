22dc329d1a86e80ca23aa4b519a2904271f38ac1
HomeDesktop <ajy720@gmail.com>
2020-04-11 23:37:42 +0900

    BOJ 11279 heap TLE

--- BOJ/11279.py
+++ BOJ/11279.py
@@ -1,24 +1,45 @@
 import sys
 
-def lower_bound(k):
-    left = 0
-    right = len(arr)
-     
-    while left < right:
-        mid = (left+right)//2
-     
-        if arr[mid] < k: left = mid + 1
-        else: right = mid
-    
-    return right
-
 if __name__ == "__main__":
-    n = int(input())
-    arr = []
-    for _ in range(n):
+    arr = [None]
+    l = 0
+    for _ in range(int(input())):
         o = int(sys.stdin.readline())
         if o == 0:
-            try: print(arr.pop(-1))
-            except: print(0)
+            if not l:
+                print(0)
+                continue
+
+            arr[1], arr[-1] = arr[-1], arr[1]
+            print(arr.pop())
+
+            node = 1
+            big = 2
+            
+            while big <= l:
+                try:
+                    if arr[big] < arr[big+1]: big += 1
+                except:
+                    pass
+
+                if arr[node] < arr[big]:
+                    arr[node], arr[big] = arr[big], arr[node]
+                    node = big
+                    big *= 2
+                else:
+                    break
+
+            l-=1
+
         else:
-            arr.insert(lower_bound(o), o)
+            arr.append(o)
+            l += 1
+            node = l
+            while node//2:
+                if arr[node//2] < arr[node]:
+                    arr[node//2], arr[node] = arr[node], arr[node//2]
+                    node //= 2
+                else: 
+                    break
+
+            
