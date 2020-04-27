f154d418cdee88bdf7f96a370dc2091ba9cadbba
AnHyeonSeok <ajy720@gmail.com>
2020-04-27 15:31:03 +0900

    BOJ 1100 optimize

--- BOJ/1102.py
+++ BOJ/1102.py
@@ -1,9 +1,2 @@
-if __name__ == "__main__":
-    arr = [input() for _ in range(8)]
-    cnt = 0
-    for i in range(8):
-        for j in range(8):
-            if abs(i - j) % 2 == 0 and arr[i][j] == "F":
-                cnt += 1
-
-    print(cnt)
+arr = [input() for _ in range(8)]
+print(sum([arr[i][i % 2 :: 2].count("F") for i in range(8)]))
