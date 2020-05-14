897f7031ea61fdef5b1789eae8d5e7370a54a9bd
HomeDesktop <ajy720@gmail.com>
2020-05-14 22:19:48 +0900

    BOJ 2875 sc 60B

--- BOJ/2875.py
+++ BOJ/2875.py
@@ -1,17 +1,2 @@
-n, m, k = map(int, input().split())
-
-n //= 2
-
-if n > m:
-    k -= (n - m)*2
-    n = m
-else:
-    k -= (m-n)
-    m = n
-
-while k > 0:
-    n -= 1
-    m -= 1
-    k -= 3
-
-print(m)
+n,m,k=map(int,input().split())
+print(min(n//2,m,(n+m-k)//3))
