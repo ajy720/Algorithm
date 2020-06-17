28c60a866cbcbcecc21d6304a0092bcd24791f79
HomeDesktop <ajy720@gmail.com>
2020-06-17 21:59:19 +0900

    BOJ 2577 optimize

--- BOJ/2577.py
+++ BOJ/2577.py
@@ -3,11 +3,5 @@ b = int(input())
 c = int(input())
 
 n = str(a*b*c)
-numlist = [0]*10
-del a, b ,c
 
-for i in n:
-    numlist[int(i)] += 1
-
-for i in numlist:
-    print(i)
+print(*[n.count(str(i)) for i in range(10)], sep="\n")
