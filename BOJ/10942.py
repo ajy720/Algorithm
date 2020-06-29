148fc6807c29f40fb3312e73d506ca6f440ff55b
AnHyeonSeok <ajy720@gmail.com>
2020-06-29 11:48:23 +0900

    BOJ 10942

--- BOJ/10942.py
+++ BOJ/10942.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 def is_palindrom(s, e):
     global arr
 
-    while s <= e:
+    while s < e:
         if arr[s] != arr[e]:
             return False
         s +=1
@@ -19,15 +19,15 @@ if __name__ == "__main__":
 
     n = int(input().strip())
     arr = [*map(int, input().split())]
-    ans = []
+    ans = set()
 
     for _ in range(int(input())):
         s, e = map(int, input().split())
 
-        if (s+e) in ans:
+        if (s, e) in ans:
             print(1)
         elif is_palindrom(s-1, e-1):
-            ans.append(s+e)
+            ans.add((s, e))
             print(1)
         else:
             print(0)
