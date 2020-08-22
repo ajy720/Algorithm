a74f091ba01b972c5704ad4d5f7f21026c0059ff
Hyeonseok <ajy720@gmail.com>
2020-08-22 18:51:46 +0900

    BOJ 9466

--- BOJ/9466.py
+++ BOJ/9466.py
@@ -1,33 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+
 def solve():
-  n = int(input())
-  arr = [0] + [*map(int, input().split())]
-  ans = 0
+    n = int(input())
+    arr = [0] + [*map(int, input().split())]
+    ans = 0
 
-  for i in range(1, n+1):
-    p = i
+    for i in range(1, n + 1):
+        p = i
+        link = []
+        while 1:
+            if arr[p] == 0:
+                if p in link:
+                    ans += len(link[:link.index(p)])
+                    break
+                else:
+                    ans += len(link)
+                    break
 
-    link = []
+            cp = p
+            link.append(p)
+            p = arr[p]
+            arr[cp] = 0
 
-    while 1:
-      if arr[arr[p]] == 0:
-        if arr[p] in link:
-          ans += len(link[:link.index(arr[p])])
-        else:
-          ans += len(link)
-        
-        break
+    return ans
 
-      link.append(p)     
-      cp = p
-      arr[cp] = 0
-      
-      p = arr[p]
-
-
-  
-  return ans
-  
 
 if __name__ == "__main__":
     for _ in range(int(input())):
-      print(solve())
+        print(solve())
