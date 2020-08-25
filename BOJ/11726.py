616ba0ff34b88e9672a6473e525699ef3eac8816
An <ajy720@gmail.com>
2020-08-25 23:49:27 +0900

    BOJ 11726 solving

---
+++ BOJ/11726.py
@@ -0,0 +1,23 @@
+def nCm(n, m):
+    if m == 0: return 1
+    if m > n or m < 0: return 0
+
+    if m > n//2: m = n-m
+
+    son = mom = 1
+
+    for i in range(0, m): 
+        son = son * (n-i)
+        mom = mom * (i+1)
+
+    return son//mom
+
+if __name__ == "__main__":
+    n = int(input())
+    ans = 0
+
+    for i in range(0, n, 2):
+        v, h = i, n-i
+
+
+
