527dc9da5a64a1adc58a79145ba3882dca94a9dc
AnHyeonSeok <ajy720@gmail.com>
2020-11-12 14:35:42 +0900

    BOJ 9576 Greedy

--- BOJ/9576.py
+++ BOJ/9576.py
@@ -1,38 +1,20 @@
 import sys
 
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-def solve(i):
-    for j in range(arr[i][0], arr[i][1]+1):
-        # 만약 j번째 책을 받은 사람이 없다면
-        if books[j] == -1:
-            # j번째 책을 i번째 사람에게 배정
-            books[j] = i
-            # j번째 책은 이미 거쳐갔으니 앞 부분 축약
-            arr[i][0] += 1
-            return True
-
-        # 그렇지 않고 j번째 책을 받은 사람이 있다면 
-        else:
-            # j번째 책을 배정받은 사람에게
-            # j+1번째 책을 배정해주고
-            if solve(books[j]): 
-                # i번째 사람이 j번째 책을 배정받는다
-                books[j] = i
-                arr[i][0] += 1
-                return True
-    
-    return False
-    
 
 for _ in ' '*int(input()):
     n, m = map(int, input().split())
 
     arr = [[*map(int, input().split())] for _ in ' '*m]
-    books = [-1] + [-1] * n
+    arr.sort(key=lambda x : x[1])
+    books = [False] * (n+1)
+    ans = 0
 
-    ans = sum([solve(i) for i in range(m)])
+    for a, b in arr:
+        for j in range(a, b+1):
+            if not books[j]:
+                books[j] = True
+                ans += 1 
+                break
+            
     print(ans)
-
-    del(arr, books, n, m, ans)
