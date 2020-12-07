9a40d0c5a21f537e5ec857e6397bfd66446f4263
HyeonSeok <ajy720@gmail.com>
2020-12-08 00:20:11 +0900

    BOJ 14654

--- BOJ/14654.py
+++ BOJ/14654.py
@@ -7,22 +7,32 @@ if __name__ == "__main__":
     arrA = [*map(int, input().split())]
     arrB = [*map(int, input().split())]
 
-    winning = True # False가 A팀, True가 B팀
-    cnt = [0, 0] # 현재 연승 카운트, cnt[False] = cnt[0], cnt[True] = cnt[1]
-    maxCnt = [0, 0] # 최대 연승 카운트, cnt[False] = cnt[0], cnt[True] = cnt[1]
+    winning = True # 연승 state | False가 A팀, True가 B팀
+    cnt = 0
+    maxCnt = 0
 
     for a, b in zip(arrA, arrB):
         if a == b:
-            # 비겼다면 연승 카운트를 반대편에게
-            if winning:
-                winning = False
-            else:
-                winning = True
-        else:
-            # 이긴 팀에게 연승 카운트를 제공
-            if win[a+b] == a:
-                winning = False
-            elif win[a+b] == b:
-                winning = True
+            maxCnt = max(maxCnt, cnt)
 
-        cnt[winning] += 1
+            cnt = 1
+            # 비겼다면 연승 카운트를 반대편에게
+            winning ^= True
+
+        else:
+            if win[a+b] == a and not winning: # A팀이 연승
+                cnt += 1
+
+            elif win[a+b] == b and winning: # B팀이 연승
+                cnt += 1
+
+            else: # A든 B든 연승이 끊기는 경우
+                maxCnt = max(maxCnt, cnt)
+
+                cnt = 1
+                # 연승 카운트를 반대편에게
+                winning ^= True
+
+    maxCnt = max(maxCnt, cnt)
+
+    print(maxCnt)
