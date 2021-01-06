0814c233edbae41cd839f0f7b1382b78e0518bdd
HyeonSeok <ajy720@gmail.com>
2021-01-06 15:32:42 +0900

    K번째수

---
+++ Programmers/level 1/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    return [sorted(array[i-1:j])[k-1] for i, j, k in commands]
+
+
+if __name__ == "__main__":
+    array = [1, 5, 2, 6, 3, 7, 4]
+
+    commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+    print(solution(array, commands))
