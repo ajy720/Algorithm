94c32da9d60569dffbd1cd3147a5220f65426dc7
Hyeonseok An <ajy720@gmail.com>
2024-10-07 17:02:58 +0900

    BOJ 10989 메모리 초과

--- BOJ/10989.py
+++ BOJ/10989.py
@@ -2,22 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-# arr = [0]*10001
-d = {}
+arr = [0]*10001
 
 for i in range(int(input())):
-    # arr[int(input())] += 1
-    key = input()
-    if key in d:
-        d[key] += 1
-    else:
-        d[key] = 1
+    arr[int(input())] += 1
 
-
-# for i, a in enumerate(arr):
-#     if a == 0: continue
-#     print(f"{i}\n"*a, end="")
-
-for num, cnt in sorted(d.items()):
-    print(f"{num}\n"*cnt, end="")
-    
+for i in range(1, 10001):
+    if arr[i] == 0: continue
+    for _ in range(arr[i]):
+        print(i)
