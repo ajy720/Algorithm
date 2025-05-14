0a2904745775c8441ccdbcd99eab1c38889bb067
Hyeonseok <ajy720@gmail.com>
2025-05-14 22:38:44 +0900

    BOJ 11005

---
+++ BOJ/11005.py
@@ -0,0 +1,46 @@
+table = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+
+if __name__ == "__main__":
+    s, base = map(int, input().split())
+    res = ""
+    while s:
+        res = table[s % base] + res
+        s //= base
+    print(res)
